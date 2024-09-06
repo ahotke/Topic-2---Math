04b24d26e2638573af3d3f30c1e4168e9da0045a
Ashton Hotke <339741886@GoTVDSB.ca>
2024-09-06 09:16:34 -0400

    Add project files.

---
+++ Topic 2 - Math/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Topic_2___Math
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I will now count my chickens");
+            Console.WriteLine("Hens:" + (10 + 5 * 2));
+            Console.WriteLine("Roosters:" + (25 + 30 / 6));
+            Console.WriteLine("");
+            Console.WriteLine("Now I will count the eggs:");
+            Console.WriteLine(11.0 / 2);
+            Console.WriteLine("");
+            Console.WriteLine("Is it true that 3 + 2 < 5 - 7?");
+            Console.WriteLine(3 + 2 < 5 - 7);
+            Console.WriteLine("");
+            Console.WriteLine("What is 3 + 2? " + (3 + 2));
+            Console.WriteLine("What is 5 - 7? " + (5 - 7));
+            Console.WriteLine("");
+            Console.WriteLine("Oh, that's why its " + (3 + 2 < 5 - 7));
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("This calculates how much money you earn working 8 hours at $21/hour");
+            Console.WriteLine("You made: $" + (21 * 8));
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("This calculates how each egg costs if a dozen is worth $6");
+            Console.WriteLine("One egg costs $" + (6.0 / 12));
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("This calculates the tax on an item worth $79.99");
+            Console.WriteLine("The tax is: $" + (79.99 * 0.13));
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("This calculates the cm in 5'6");
+            Console.WriteLine("Number of Cm: " + ((5 * 12 + 6) * 2.54));
+
+
+
+        }
+    }
+}
+
+
+//Adding a decimal place after a whole number converts it into a floating point value or 'Float'
